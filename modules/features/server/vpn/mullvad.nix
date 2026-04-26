d8d30880be80cb13ce4af673d04b8823caf91e25
nimeses <nilshasenthal@gmail.com>
2026-04-26 19:37:48 +0200

    fix: this time fr fr

--- modules/features/server/vpn/mullvad.nix
+++ modules/features/server/vpn/mullvad.nix
@@ -12,6 +12,15 @@
 
       dns = ["10.64.0.1"];
 
+      postUp = ''
+        ip rule add to 100.64.0.0/10 priority 100 table main
+        ip -6 rule add to fd7a:115c:a1e0::/48 priority 100 table main
+      '';
+      preDown = ''
+        ip rule del to 100.64.0.0/10 priority 100 table main 2>/dev/null || true
+        ip -6 rule del to fd7a:115c:a1e0::/48 priority 100 table main 2>/dev/null || true
+      '';
+
       peers = [
         {
           publicKey = "HQHCrq4J6bSpdW1fI5hR/bvcrYa6HgGgwaa5ZY749ik=";
