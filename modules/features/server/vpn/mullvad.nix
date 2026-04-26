2413fa011a5674201ad62dbd58439c2aa045687e
nimeses <nilshasenthal@gmail.com>
2026-04-26 15:51:58 +0200

    fix: mullvad switch acually killed tailscale

--- modules/features/server/vpn/mullvad.nix
+++ modules/features/server/vpn/mullvad.nix
@@ -28,6 +28,8 @@
       iptables -A KILLSWITCH -o tailscale0 -j RETURN
       iptables -A KILLSWITCH -m mark --mark 0xca6c -j RETURN
       iptables -A KILLSWITCH -m addrtype --dst-type LOCAL -j RETURN
+      iptables -A KILLSWITCH -d 185.213.155.73/32 -p udp --dport 51820 -j RETURN
+      iptables -A KILLSWITCH -p udp --dport 41641 -j RETURN
       iptables -A KILLSWITCH -j REJECT
       iptables -C OUTPUT -j KILLSWITCH 2>/dev/null || iptables -I OUTPUT -j KILLSWITCH
 
@@ -36,6 +38,8 @@
       ip6tables -A KILLSWITCH -o tailscale0 -j RETURN
       ip6tables -A KILLSWITCH -m mark --mark 0xca6c -j RETURN
       ip6tables -A KILLSWITCH -m addrtype --dst-type LOCAL -j RETURN
+      ip6tables -A KILLSWITCH -d 185.213.155.73/32 -p udp --dport 51820 -j RETURN
+      ip6tables -A KILLSWITCH -p udp --dport 41641 -j RETURN
       ip6tables -A KILLSWITCH -j REJECT
       ip6tables -C OUTPUT -j KILLSWITCH 2>/dev/null || ip6tables -I OUTPUT -j KILLSWITCH
     '';
