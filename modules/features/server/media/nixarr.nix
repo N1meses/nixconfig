1841a23da79aeb7803a8cc8328b704b323164f38
nimeses <nilshasenthal@gmail.com>
2026-04-28 14:28:05 +0200

    fix: added airvpn and port forwarding to nixarr.nix

--- modules/features/server/media/nixarr.nix
+++ modules/features/server/media/nixarr.nix
@@ -1,10 +1,14 @@
 {inputs, ...}: {
-  flake.modules.nixos.nixarr = {config, ...}: {
+  flake.modules.nixos.nixarr = {
+    config,
+    lib,
+    ...
+  }: {
     imports = [
       inputs.nixarr.nixosModules.default
     ];
 
-    sops.secrets.mullvad-wg-conf = {};
+    sops.secrets.airvpn-wg-conf = {};
 
     nixarr = {
       enable = true;
@@ -13,12 +17,15 @@
 
       vpn = {
         enable = true;
-        wgConf = config.sops.secrets.mullvad-wg-conf.path;
+        wgConf = config.sops.secrets.airvpn-wg-conf.path;
+        openUdpPorts = [56599];
+        openTcpPorts = [56599];
       };
 
       transmission = {
         enable = true;
         vpn.enable = true;
+        peerPort = 56599;
         extraSettings = {
           "ratio-limit" = 0;
           "ratio-limit-enabled" = true;
@@ -26,6 +33,13 @@
           "speed-limit-up-enabled" = true;
           "idle-seeding-limit" = 0;
           "idle-seeding-limit-enabled" = true;
+          "cache-size-mb" = 64;
+          "peer-limit-global" = 3000;
+          "peer-limit-per-torrent" = 100;
+          "dht-enabled" = true;
+          "pex-enabled" = true;
+          "utp-enabled" = true;
+          "port-forwarding-enabled" = false;
         };
       };
 
@@ -38,10 +52,7 @@
       lidarr.enable = true;
       readarr.enable = true;
       bazarr.enable = true;
-      autobrr = {
-        enable = true;
-        vpn.enable = true;
-      };
+      autobrr.enable = true;
       jellyseerr.enable = true;
     };
 
