a631c98aa1ba02f107d18cde90fdb9391cafea13
nimeses <nilshasenthal@gmail.com>
2026-05-31 21:20:58 +0200

    feat: made noctalia a generator which takes in a json and make mkDefault values

--- modules/features/desktop/noctalia/noctalia.nix
+++ modules/features/desktop/noctalia/noctalia.nix
@@ -6,43 +6,45 @@
 }: let
   flakeConfig = config;
 in {
-  flake.lib.mkNoctaliaNiri = cmd:
-    ["noctalia-shell" "ipc" "call"] ++ (lib.splitString " " cmd);
+  flake = {
+    lib.mkNoctaliaNiri = cmd:
+      ["noctalia-shell" "ipc" "call"] ++ (lib.splitString " " cmd);
 
-  flake.lib.mkNoctaliaHypr = cmd: "exec, noctalia-shell ipc call ${cmd}";
+    lib.mkNoctaliaHypr = cmd: "exec, noctalia-shell ipc call ${cmd}";
 
-  flake.lib.mkNoctaliaMango = cmd: "spawn, noctalia-shell ipc call ${cmd}"; 
+    lib.mkNoctaliaMango = cmd: "spawn, noctalia-shell ipc call ${cmd}";
 
-  flake.modules = {
-    nixos.noctalia = {...}: {
-      nix.settings = {
-        substituters = ["https://noctalia.cachix.org"];
-        trusted-public-keys = ["noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="];
+    modules = {
+      nixos.noctalia = {...}: {
+        nix.settings = {
+          substituters = ["https://noctalia.cachix.org"];
+          trusted-public-keys = ["noctalia.cachix.org-1:pCOR47nnMEo5thcxNDtzWpOxNFQsBRglJzxWPp3dkU4="];
+        };
       };
-    };
 
-    homeManager.noctalia = {...}: {
-      imports = [
-        inputs.noctalia.homeModules.default
-        flakeConfig.flake.modules.homeManager.noctaliaSettings
-      ];
-
-      programs.niri.settings = {
-        spawn-at-startup = [{argv = ["noctalia-shell"];}];
-        layer-rules = [
-          {
-            matches = [{namespace = "^noctalia-wallpaper.*";}];
-            place-within-backdrop = true;
-          }
+      homeManager.noctalia = {...}: {
+        imports = [
+          inputs.noctalia.homeModules.default
+          flakeConfig.flake.modules.homeManager.noctaliaSettings
         ];
+
+        programs.niri.settings = {
+          spawn-at-startup = [{argv = ["noctalia-shell"];}];
+          layer-rules = [
+            {
+              matches = [{namespace = "^noctalia-wallpaper.*";}];
+              place-within-backdrop = true;
+            }
+          ];
+        };
+        wayland.windowManager.hyprland.settings.exec-once = ["noctalia-shell"];
+
+        features.compositors.mango.autoStart = ''
+          noctalia-shell &
+        '';
+
+        programs.noctalia-shell.enable = true;
       };
-      wayland.windowManager.hyprland.settings.exec-once = ["noctalia-shell"];
-
-      features.compositors.mango.autoStart = ''
-        noctalia-shell &
-      '';
-
-      programs.noctalia-shell.enable = true;
     };
   };
 }
