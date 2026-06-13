8b6367826b702e2114fd37faa983f45e944bb882
N1meses <nilshasenthal@gmail.com>
2026-06-13 19:26:38 +0200

    fix: disable getty in ly

--- modules/features/desktop/services/ly.nix
+++ modules/features/desktop/services/ly.nix
@@ -11,8 +11,14 @@
       security.pam.services.ly.enableGnomeKeyring = true;
     };
 
-    finix.ly = {modules, ...}: {
+    finix.ly = {
+      modules,
+      lib,
+      ...
+    }: {
       imports = [modules.ly];
+      services.getty.enable = lib.mkForce false;
+
       services.ly = {
         enable = true;
         settings = {
