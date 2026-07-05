d4ebc75d9e1277e824fcff345aa019ed25cf41d3
N1meses <nilshasenthal@gmail.com>
2026-07-06 01:19:12 +0200

    fix: icons on finix

--- modules/features/desktop/services/session.nix
+++ modules/features/desktop/services/session.nix
@@ -9,6 +9,9 @@ _: {
     services.seatd.enable = true;
     finit.runlevel = 3;
 
+    xdg.icons.enable = true;
+    environment.pathsToLink = ["/share/applications" "/share/mime"];
+
     programs.doas.enable = true;
 
     security.pam.services = let
