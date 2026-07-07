300e96a39138561c9d3a4a7813dfb7b0865fbac6
N1meses <nilshasenthal@gmail.com>
2026-07-07 08:49:19 +0200

    fix: /boot not being mounted

--- modules/hosts/icarus/diskoIcarus.nix
+++ modules/hosts/icarus/diskoIcarus.nix
@@ -58,12 +58,16 @@ in {
   flake.modules.finix.diskoIcarus = {lib, ...}: {
     fileSystems = lib.mkMerge [
       fileSystems
-      {"/persist".neededForBoot = true;}
       {
-      "/tmp" = {
+        "/persist".neededForBoot = true;
+        "/tmp" = {
           device = "tmpfs";
           fsType = "tmpfs";
           options = ["nosuid" "nodev" "mode=1777"];
+        };
+        "/boot" = {
+          device = lib.mkForce "/dev/nvme0n1p1";
+          noCheck = true;
         };
       }
     ];
