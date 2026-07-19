f8e131db044a64f7c167389528d71c89f2a0ce4e
N1meses <nilshasenthal@gmail.com>
2026-07-19 21:45:10 +0200

    fix: maybe?

--- modules/features/server/monitoring.nix
+++ modules/features/server/monitoring.nix
@@ -17,6 +17,7 @@ in
       services.netdata.enable = true;
       services.netdata.package = pkgs.netdata.override { withCloudUi = true; };
       services.netdata.config.plugins."scripts.d" = "no";
+      services.netdata.config.directories.config = "/etc/netdata";
       networking.firewall.interfaces."tailscale0".allowedTCPPorts = [ 19999 ];
 
       services.netdata.config.db = lib.mkIf isParent {
