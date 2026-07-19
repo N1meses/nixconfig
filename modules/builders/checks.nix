f101de2b503492fbe506bc163ac11c2aab45dde3
N1meses <nilshasenthal@gmail.com>
2026-07-19 22:06:09 +0200

    feat: add finix builds to ci

--- modules/builders/checks.nix
+++ modules/builders/checks.nix
@@ -1,5 +1,9 @@
 { lib, config, ... }: {
-  checks = lib.mapAttrs' (
-    name: nixos: lib.nameValuePair "host-${name}" nixos.config.system.build.toplevel
-  ) (lib.filterAttrs (name: _: !(lib.hasSuffix "Minimal" name)) config.nixosConfigurations);
+  checks =
+    lib.mapAttrs' (name: nixos: lib.nameValuePair "host-${name}" nixos.config.system.build.toplevel) (
+      lib.filterAttrs (name: _: !(lib.hasSuffix "Minimal" name)) config.nixosConfigurations
+    )
+    // lib.mapAttrs' (
+      name: finix: lib.nameValuePair "finix-${name}" finix.config.system.build.toplevel
+    ) config.finixConfigurations;
 }
