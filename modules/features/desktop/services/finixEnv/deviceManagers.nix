e011d30e7f2ec45c46aa02d728499cd02327ce00
N1meses <nilshasenthal@gmail.com>
2026-07-20 22:42:44 +0200

    fix: no event broadcast fom mdevd

--- modules/features/desktop/services/finixEnv/deviceManagers.nix
+++ modules/features/desktop/services/finixEnv/deviceManagers.nix
@@ -1,5 +1,8 @@
 _: {
   aspects.devUdev.finix = _: { services.udev.enable = true; };
-  aspects.devMdevd.finix = _: { services.mdevd.enable = true; };
+  aspects.devMdevd.finix = _: {
+    services.mdevd.enable = true;
+    services.mdevd.nlgroups = 4;
+  };
   aspects.devGardendevd.finix = _: { services.gardendevd.enable = true; };
 }
