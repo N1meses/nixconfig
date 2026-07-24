231cd7a48aae7011ccd47c002caa9f6efed601cf
N1meses <nilshasenthal@gmail.com>
2026-07-24 23:23:50 +0200

    update finix and remove luks workaround

--- modules/features/desktop/services/finixEnv/luks.nix
+++ modules/features/desktop/services/finixEnv/luks.nix
@@ -1,17 +1,6 @@
 _: {
-  aspects.luks.finix =
-    {
-      pkgs,
-      lib,
-      config,
-      ...
-    }:
-    {
-      boot.initrd.supportedFilesystems.luks.enable = true;
-      boot.supportedFilesystems.luks.enable = true;
-
-      boot.initrd.finit.tasks.luks.script = lib.mkBefore ''
-        ${pkgs.busybox}/bin/loadkmap < ${config.hardware.console.binaryKeyMap} || true
-      '';
-    };
+  aspects.luks.finix = _: {
+    boot.initrd.supportedFilesystems.luks.enable = true;
+    boot.supportedFilesystems.luks.enable = true;
+  };
 }
